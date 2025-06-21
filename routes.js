3310c07a01785630cdea86fac5522cec0d383efd
Monsters87 <134213480+Monsters87@users.noreply.github.com>
2025-06-21 13:13:17 +0100

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRumours, getPlayer } = require('./scraper');
-
-router.get('/rumours', getRumours);
-router.get('/player/:name', getPlayer);
-
-module.exports = router;
