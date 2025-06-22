9ff4b756211b114ecb5bbb59fceb065b30ea6ace
Monsters87 <134213480+Monsters87@users.noreply.github.com>
2025-06-22 17:03:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes");
-require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+// Allow Netlify frontend
+app.use(
+  cors({
+    origin: "https://ibroxintel.netlify.app",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.use(express.json());
 app.use("/api", routes);
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Keeps Railway container alive
-setInterval(() => {}, 1 << 30);
